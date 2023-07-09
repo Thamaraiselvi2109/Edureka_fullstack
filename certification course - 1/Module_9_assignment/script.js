d837d7c5118a62fb64569f5184104c7319648ed7
Thamarai <thamaraiselvig2109@gmail.com>
2023-07-09 20:27:49 +0530

    module 9

---
+++ certification course - 1/Module_9_assignment/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#button').on('click',function(){
+        $.ajax({
+            type: "GET",
+            url: $(this).attr('data-src'),
+            success: function (response) {  
+                $('#demo').html(response)
+            }
+        });
+    })
+});
+$
