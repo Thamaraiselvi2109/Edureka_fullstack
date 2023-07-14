0398b2153a54f0aeccdb5f5bf5456cd6510a475d
Thamarai <thamaraiselvig2109@gmail.com>
2023-07-14 11:41:43 +0530

    jquery module 1

---
+++ JQuery UI development/Module_1_assignment/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    //tag selector
+    $('.jumbotron').css({
+        'margin-top':40,
+        'background-color':'#000000',
+        'color':'white'
+    })
+    $('input').css({
+        'width':"95%",
+        'background-color':'#681576',
+        'color':'white'
+    })
+    //attribute selector
+    $('.btn').css({
+        'border-radius':25,
+        'color':"black",
+        "border":"2px solid grey",
+        'background-color':'lightgrey',
+        'margin-top':"20px",
+    })
+
+
+    //attribute selector
+    $('#heading').css({
+        'text-align':'center',
+        'color':'aliceblue',
+        'padding-bottom':50
+    })
+
+});
